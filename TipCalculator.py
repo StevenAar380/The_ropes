2322b0962b159247eb41f6e929797b89eea6755b
StevenAar380 <stevemarkenson380@gmail.com>
2025-07-08 09:57:08 +0100

    Welcome message addded

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,9 +1,11 @@
+
 def tip_calculator(amount, percentage):
     tip = amount * percentage/100
     total = amount + tip 
     return total
 
 print("hi")
+print(f"welcome to the tip calculator")
 tipamount = int(input("Enter tip amount>>> "))
 tipercentage = int(input("Enter tip percentage>>> "))
 print(f"The total cost is {tip_calculator(tipamount, tipercentage)}")
