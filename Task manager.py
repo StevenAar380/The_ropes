a20604e83195ef971dc3b299ed3cfa93c37b370d
StevenAar380 <stevemarkenson380@gmail.com>
2025-07-08 15:43:47 +0100

    Random name generator (array p2)

--- Task manager.py
+++ Task manager.py
@@ -1,10 +1,14 @@
-Mclaren = ["P1", "720s","750s","senna", "senna gtr"]
+#Random Selector
+import random
+done = 0
+list = []
 
-def car_generator(luckyno):
-    luckyno -= 1
-    return Mclaren[luckyno]
-
-
-print("Congratulations you got a free Mclaren!")
-luckyno = int(input("Enter your lucky number (1-5)>>> "))
-print(f"Well done sir, you got a {car_generator(luckyno)}")
+print("Hello there")
+while done != 1:
+    temp = input("Enter name (type null when complete!) >>> ")
+    list.append(temp)
+    print(list)
+    if temp.lower() == "null":
+        done = 1
+        rand = random.randint(0,len(list))
+        print("Your random person is", list[rand])
