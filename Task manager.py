71194c308deef683f772b7b902e48b5aa809c4e7
StevenAar380 <stevemarkenson380@gmail.com>
2025-07-08 15:30:15 +0100

    Mclaren gift program (array p1)

--- Task manager.py
+++ Task manager.py
@@ -1,3 +1,10 @@
 Mclaren = ["P1", "720s","750s","senna", "senna gtr"]
 
-print("hi")
+def car_generator(luckyno):
+    luckyno -= 1
+    return Mclaren[luckyno]
+
+
+print("Congratulations you got a free Mclaren!")
+luckyno = int(input("Enter your lucky number (1-5)>>> "))
+print(f"Well done sir, you got a {car_generator(luckyno)}")
