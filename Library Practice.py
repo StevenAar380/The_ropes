bf4f9be963d3a9496953691bddd626a75192373d
StevenAar380 <stevemarkenson380@gmail.com>
2025-08-01 10:59:54 +0530

    added random shuffle

--- Library Practice.py
+++ Library Practice.py
@@ -2,8 +2,16 @@
 import random
 
 #List
-teams = ("alpha", "beta", "charlie", "delta")
+teams = ["alpha", "beta", "charlie", "delta"]
 numb = random.randint(4,15)
+
 
 #randomly list a team
 print(f"you are in the {numb}th {random.choice(teams)} squad")
+
+
+print(random.shuffle(teams)) #prints None
+#but
+random.shuffle(teams)
+for team in teams:
+    print(team)
