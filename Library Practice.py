5d06c536d5e5ac9d263c0fe7fc7426ecd9e69479
StevenAar380 <stevemarkenson380@gmail.com>
2025-08-01 11:05:15 +0530

    played with sys module XD

--- Library Practice.py
+++ Library Practice.py
@@ -1,5 +1,6 @@
 #Importing Libraries
 import random
+import sys
 
 #List
 teams = ["alpha", "beta", "charlie", "delta"]
@@ -9,9 +10,7 @@ numb = random.randint(4,15)
 #randomly list a team
 print(f"you are in the {numb}th {random.choice(teams)} squad")
 
-
-print(random.shuffle(teams)) #prints None
-#but
 random.shuffle(teams)
 for team in teams:
     print(team)
+
