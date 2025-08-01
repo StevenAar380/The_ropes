4c260f2e5b0b5b1617a7f69680fecf7b487d550a
StevenAar380 <stevemarkenson380@gmail.com>
2025-08-01 10:49:42 +0530

    Added random int

--- Library Practice.py
+++ Library Practice.py
@@ -3,6 +3,7 @@ import random
 
 #List
 teams = ("alpha", "beta", "charlie", "delta")
+numb = random.randint(4,15)
 
 #randomly list a team
-print(f"you are {random.choice(teams)} team")
+print(f"you are in the {numb}th {random.choice(teams)} squad")
