2783917a0591a8b0b29792d4876677597a758707
StevenAar380 <stevemarkenson380@gmail.com>
2025-08-02 13:21:29 +0530

    Added API practice

--- API Practice.py
+++ API Practice.py
@@ -1,4 +1,7 @@
 import requests
 import json
+from bs4 import BeautifulSoup
+response = requests.get("https://www.classcharts.com/mobile/student#5529766,announcements")
+soup = BeautifulSoup(response.text, "html.parser")
 
-
+print(soup)
