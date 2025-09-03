f425ec8aad060895fde5a85c1a521466fd418f8e
StevenAar380 <stevemarkenson380@gmail.com>
2025-09-03 21:01:35 +0100

    added menu ui

---
+++ lost item logger.py
@@ -0,0 +1,45 @@
+import sys
+
+def addlostitem():
+    print("function called 1")
+
+
+def lostItemSearch():
+    print("function called 2")
+
+
+
+def showAllItems():
+    print("function called 3")
+
+
+
+
+def menu():
+    print("What would you like to do:")
+    print("1 Add lost items")
+    print("2 Item search by keyword")
+    print("3 Show all items")
+    print("4 exit")
+    choice = input("(choose a number)>>>")
+    if choice == "1":
+        addlostitem()
+    elif choice == "2":
+        lostItemSearch()
+    elif choice == "3":
+        showAllItems()
+    elif choice == "4":
+        print("Thank you for using the service")
+        sys.exit()
+    else:
+        print("invalid output")
+        menu()
+
+
+def greeting():
+    print("=================================================================================")
+    print("Welcome to the tfl lost item logger")
+    print("=================================================================================")
+    menu()
+
+greeting()
