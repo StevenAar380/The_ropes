77233c9f9796605bd7b0bf11db028bb2cd43f572
StevenAar380 <stevemarkenson380@gmail.com>
2025-09-12 20:20:48 +0100

    all bugs resolved, program is functional

--- Rani's guests.py
+++ Rani's guests.py
@@ -13,34 +13,42 @@ def main():
 def nameinput():
     total = 0
     yperson = ""
+    contage = 100
     total18 = 0
     while True:
-        tname = input("Enter the name: ")
-        tage = int(input("Enter age: "))
-        if tage < tage:
-            yperson = tname
-        if tage >= 18:
-            total18 += 1
-        if tname == "done" or tage == "done":
+        tname = input("Enter the name (type done when finished): ")
+        if tname == "done":
             break
+        tage = input("Enter age(number only): ")
+        while tage.isdigit() == False:
+            print("I said enter a number you failiure")
+            tage = input("Enter age(number only): ")
+        tage = int(tage)
+        if tage < contage:
+            contage = tage
+            yperson = tname
+        elif tage >= 18:
+            total18 += 1
         total += 1
         guest[tname] = tage
-        dictAdd(tname, tage, total,yperson, total18)
+    dictAdd(tname, tage, total,yperson, total18)
     
 def dictAdd(name, age, total,yperson, total18):
     for name, age in guest.items():
         print(f"""
         -----------------------------------
-        Student: {name} | Grade: {age}             
+        Student: {name} | Age: {age}             
         """)
 
     stats(total,yperson, total18)
     
 
 def stats(total,yperson, total18):
+    print("=" * 72)
     print(f"The total number of guests are {total}")
     print(f"the youngest person was {yperson}")
     print(f"the total number of people over 18 was {total18}")
+    print("=" * 72)
     sys.exit()
 
 main()
